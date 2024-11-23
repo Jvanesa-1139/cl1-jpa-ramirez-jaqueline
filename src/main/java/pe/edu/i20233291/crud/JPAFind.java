b73d99cb7cbbb7fab81304b991cbb356f81361a6
jvanesa-1139 <jvanessaec@gmail.com>
2024-11-23 01:04:06 -0500

    Primer commit

--- src/main/java/pe/edu/i20233291/crud/JPAFind.java
+++ src/main/java/pe/edu/i20233291/crud/JPAFind.java
@@ -3,18 +3,35 @@ package pe.edu.i20233291.crud;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import pe.edu.i20233291.entity.City;
 import pe.edu.i20233291.entity.Country;
 
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class JPAFind {
+
     public static void main(String[] args) {
 
+        // referenciar al EMF y EM
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("world");
         EntityManager em = emf.createEntityManager();
 
-        Country country = em.find(Country.class, "CTY");
+        // buscar el país con código
+        Country country = em.find(Country.class, "PER");
 
-        em.getTransaction().begin();
-        em.persist(country);
-        em.getTransaction().commit();
+        // Filtrar las ciudades con población mayor a 700,000
+        List<City> cities = country.getCity().stream()
+                .filter((city) -> {
+                    return city.getPopulation() > 700000;
+                })
+                .collect(Collectors.toList());
+
+        cities.forEach((city) -> {
+            System.out.println(city.getName());
+        });
+
     }
+
 }
